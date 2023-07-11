8d95d024e6ad51de3407b08bb4cd752802fbf89b
Reehan Sarmah <reehansarmah8473@gmail.com>
2023-07-12 00:10:49 +0530

    comments added

--- network_1.py
+++ network_1.py
@@ -13,6 +13,7 @@ from keras.losses import BinaryCrossentropy
 # For Hidden Layers
 # - Mostly use ReLU (faster than Sigmoid)
 
+# Design the model
 model = Sequential([
     Dense(units=3, activation='relu'),
     Dense(units=1, activation='sigmoid')
@@ -24,11 +25,12 @@ x = np.array([[200, 17],
               [212, 18]])
 y = np.array([1, 0, 0, 1])
 
+# compile the model with correct loss function
 model.compile(loss=BinaryCrossentropy())
-model.fit(x, y, epochs=100)
+model.fit(x, y, epochs=100)  # Fit the dataset into the model
 temperature = int(input('Enter temperature (in deg celsius)'))
 duration = int(input('Enter duration (in mins)'))
-percent = model.predict([[temperature, duration]])
+percent = model.predict([[temperature, duration]])  # predict data
 
 yhat = 0
 ans = True
